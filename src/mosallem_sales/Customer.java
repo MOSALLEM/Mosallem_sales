c92d30e4a9d08983e4934d2e68c0995abe8a0296
MOSALLEM AHMED <mos18005@spengergasse.at>
2019-04-09 00:07:45 +0200

    MOSALLEM: CUSTOMER CLASS

---
+++ src/mosallem_sales/Customer.java
@@ -0,0 +1,27 @@
+package mosallem_sales;
+
+public class Customer {
+	int customerID;
+	String firstName;
+	
+	public Customer() {
+		
+	}
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+}
